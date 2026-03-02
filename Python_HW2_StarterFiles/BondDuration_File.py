895ae0bd1303cb8fed939bbb4bc974258d40a1bd
zz3467-sps <zz3467@columbia.edu>
2026-03-02 15:21:01 -0500

    Update BondDuration_File.py

--- Python_HW2_StarterFiles/BondDuration_File.py
+++ Python_HW2_StarterFiles/BondDuration_File.py
@@ -1,8 +1,14 @@
-import numpy as np
+def getBondDuration(y, face, couponRate, m, ppy=1):
+    n = m * ppy
+    y_period = y / ppy
+    coupon = face * couponRate / ppy
+    periods = np.arange(1, n + 1)
+    cash_flows = np.full(n, coupon)
+    cash_flows[-1] += face  
+    discount_factors = (1 + y_period) ** periods
+    pv_cash_flows = cash_flows / discount_factors
+    price = np.sum(pv_cash_flows)
+    time_years = periods / ppy
+    duration = np.sum(time_years * pv_cash_flows) / price
+    return durationimport numpy as np
 
-def getBondDuration(y, face, couponRate, m, ppy = 1):
-    if ppy == 1:
-        x = 8.51
-    if ppy == 2:
-        x = 8.42
-    return(x)
